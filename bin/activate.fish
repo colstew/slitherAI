a66777d848635af0be2572a71dac12bbc767058f
Cole Stewart <colestew@uvic.ca>
2018-05-09 11:38:27 -0700

    added universe submodule

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/cole/slitherAI"
+set -gx VIRTUAL_ENV "/home/colestew/slitherAI"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
